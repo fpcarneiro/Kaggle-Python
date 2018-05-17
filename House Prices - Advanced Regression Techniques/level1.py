c9e9a7df10731b612992a80a6827d9ba8da230f7
Fernando Carneiro <fpcar@yahoo.com.br>
2018-05-17 20:25:40 -0300

    Commit #1

---
+++ House Prices - Advanced Regression Techniques/level1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+# Read the data
+train = pd.read_csv('input/train.csv')
+
+# pull data into target (y) and predictors (X)
+train_y = train.SalePrice
+predictor_cols = ['LotArea', 'OverallQual', 'YearBuilt', 'TotRmsAbvGrd']
+
+# Create training predictors data
+train_X = train[predictor_cols]
+
+my_model = RandomForestRegressor()
+my_model.fit(train_X, train_y)
+
+# Read the test data
+test = pd.read_csv('input/test.csv')
+# Treat the test data in the same way as training data. In this case, pull same columns.
+test_X = test[predictor_cols]
+# Use the model to make predictions
+predicted_prices = my_model.predict(test_X)
+# We will look at the predicted prices to ensure we have something sensible.
+print(predicted_prices)
+
+my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices})
+# you could use any filename. We choose submission here
+my_submission.to_csv('submission.csv', index=False)
