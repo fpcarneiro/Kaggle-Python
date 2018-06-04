6bb4cf5dacdf22ea56cc90f6f202d3916d36da37
Fernando Carneiro <fpcar@yahoo.com.br>
2018-06-04 00:53:10 -0300

    Commit #1

--- House Prices - Advanced Regression Techniques/house_prices.py
+++ House Prices - Advanced Regression Techniques/house_prices.py
@@ -9,7 +9,7 @@ from sklearn.kernel_ridge import KernelRidge
 from xgboost import XGBRegressor
 import lightgbm as lgb
 from sklearn.svm import SVR, LinearSVR, LinearSVC
-from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge, Ridge, SGDRegressor
+from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge, Ridge, SGDRegressor, LassoLars
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 
 import warnings
@@ -26,8 +26,10 @@ all_data = pp.concat_train_test(train.drop(['SalePrice'], axis=1), test)
 #ds = ds.drop(['Utilities'], axis=1)
 #ds = ds.drop(high_occurance_missing(ds, 0.8), axis=1)
 
-all_data = pp.convert_numeric2category(all_data)
-was_missing_columns = pp.handle_missing(all_data)
+pp.convert_numeric2category(all_data)
+was_missing_columns = pp.handle_missing(all_data, False)
+
+#more_features = pp.more_features(all_data)
 
 all_data = pp.encode(all_data)
 shrunk_columns = pp.shrink_scales(all_data)
@@ -54,6 +56,18 @@ test_X = (all_data_encoded.loc[all_data_encoded.dataset == "test"]).drop(['datas
 all_predictors = train_X.columns
 predictors = list(set(train_X.columns) - set(was_missing_columns))
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+plt.style.use('ggplot')
+lasso = Lasso(alpha=0.000507)
+lasso.fit(train_X, train_y)
+FI_lasso = pd.DataFrame({"Feature Importance":lasso.coef_}, index=train_X.columns)
+FI_lasso.sort_values("Feature Importance",ascending=False)
+FI_lasso[FI_lasso["Feature Importance"]!=0].sort_values("Feature Importance").plot(kind="barh",figsize=(15,25))
+plt.xticks(rotation=90)
+plt.show()
+
 scaler = RobustScaler()
 train_X = scaler.fit(train_X[predictors]).transform(train_X[predictors])
 test_X = scaler.transform(test_X[predictors])
@@ -69,7 +83,6 @@ test_X_reduced = sfm.transform(test_X)
 
 model_lasso = Lasso(alpha = 0.000507, random_state = 1)
 model_ridge = Ridge(alpha=10.0)
-#model_svr = SVR(C = 1.466, epsilon = 0.0322, gamma = 0.0015, kernel = 'rbf')
 model_svr = SVR(C = 15, epsilon = 0.009, gamma = 0.0004, kernel = 'rbf')
 model_ENet = ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3, max_iter = 10000)
 model_KRR = KernelRidge(alpha=0.5, kernel='polynomial', degree=2, coef0=2.5)
@@ -82,7 +95,7 @@ model_extra = ExtraTreesRegressor()
 
 model_xgb = XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
                              learning_rate=0.05, max_depth=4, 
-                             min_child_weight=1.7817, n_estimators=2500,
+                             min_child_weight=1.7817, n_estimators=3000,
                              reg_alpha=0.4640, reg_lambda=0.88,
                              subsample=0.5213, silent=1,
                              random_state =7, nthread = -1)
@@ -99,32 +112,38 @@ model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
                               feature_fraction_seed=9, bagging_seed=9,
                               min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
 
-models = []
-models.append(model_lasso)
-models.append(model_ridge)
-models.append(model_svr)
-models.append(model_ENet)
-models.append(model_KRR)
-models.append(model_byr)
-models.append(model_rforest)
-models.append(model_xgb)
-models.append(model_GBoost)
-models.append(model_lgb)
+model_lasso_lars = LassoLars(alpha = 0.000507)
 
-names = ["lasso", "ridge", "svr", "ENet", "KRR", "byr", "rforest", "xgb", "GBoost", "lgb"]
-#names = ["ridge", "xgb", "lasso", "ENet", "KRR", "GBoost",
-#         "lgb", "rforest", "svr", "lsvr", "sgd", "byr", "extra"]
-   
-scores = [(np.sqrt(pp.score_model(model, train_X_reduced, train_y))).mean() for name, model in zip(names, models)]
+#Linear Models
+models = []
+models.append(("lasso", model_lasso))
+models.append(("ridge", model_ridge))
+models.append(("svr", model_svr))
+models.append(("ENet", model_ENet))
+models.append(("KRR", model_KRR))
+models.append(("byr", model_byr))
+models.append(("rforest", model_rforest))
+models.append(("xgb", model_xgb))
+models.append(("GBoost", model_GBoost))
+models.append(("lgb", model_lgb))
+models.append(("lasso_lars", model_lasso_lars))
+models.append(("lsvr", model_lsvr))
+models.append(("sgd", model_sgd))
+models.append(("extra", model_extra))
+
+scores = []
+names = []
+for name, model in models:
+    names.append(name)
+    scores.append(np.sqrt(pp.score_model(model, train_X_reduced, train_y)).mean())
 tab = pd.DataFrame({ "Model" : names, "Score" : scores })
 tab = tab.sort_values(by=['Score'], ascending = True)
 print(tab)
 
-for model in models:
-    model.fit(train_X, train_y)
+for name, model in models:
+    model.fit(train_X_reduced, train_y)
     
-averaged_models = em.AveragingModels(models = (model_lasso, model_ENet, model_svr, model_KRR, 
-                                            model_ridge, model_byr, model_xgb, model_GBoost) )
+averaged_models = em.AveragingModels(models = [model_svr, model_byr, model_KRR])
 
 score_avg = np.sqrt(pp.score_model(averaged_models, train_X_reduced, train_y))
 print(" Averaged base models score: {:.6f} ({:.6f})\n".format(score_avg.mean(), score_avg.std()))
@@ -135,9 +154,8 @@ print(predicted_prices_averaged)
 my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices_averaged})
 my_submission.to_csv('submission_avg.csv', index=False)
 
-stacked_averaged_models = em.StackingAveragedModels(base_models = (model_ENet, model_svr, model_KRR, model_lasso,
-                                                                model_ridge),
-                                                 meta_model = model_byr)
+stacked_averaged_models = em.StackingAveragedModels(base_models = [model_svr, model_byr, model_ridge],
+                                                 meta_model = model_KRR)
 
 score_stacked_averaged = np.sqrt(pp.score_model(stacked_averaged_models, train_X_reduced, train_y))
 print(" Stacked Averaged base models score: {:.6f} ({:.6f})\n".format(score_stacked_averaged.mean(), 
@@ -155,13 +173,8 @@ model_lgb.fit(train_X_reduced, train_y)
 predicted_prices_lgb = np.expm1(model_lgb.predict(test_X_reduced))
 print(predicted_prices_lgb)
 
-model_GBoost.fit(train_X_reduced, train_y)
-predicted_prices_GBoost = np.expm1(model_GBoost.predict(test_X_reduced))
-print(predicted_prices_GBoost)
-
 predicted_prices = predicted_prices_stacked_averaged*0.7 + predicted_prices_xgboost*0.2 + predicted_prices_lgb*0.1
-
-predicted_prices = (predicted_prices_stacked_averaged*0.6 + predicted_prices_averaged*0.4)*0.7 + predicted_prices_xgboost*0.2 + predicted_prices_lgb*0.1
 
 my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': predicted_prices})
 my_submission.to_csv('submission_ensemble.csv', index=False)
+
