2d6ad4ac8d8e1167a1a7199cb9faefb98409e39f
Fernando Carneiro <fpcar@yahoo.com.br>
2018-06-21 00:28:37 -0300

    Commit #1

--- Santander Value Prediction Challenge/santander_2.py
+++ Santander Value Prediction Challenge/santander_2.py
@@ -13,12 +13,13 @@ import numpy as np
 from sklearn.preprocessing import FunctionTransformer
 import lightgbm as lgb
 import pandas as pd
+from xgboost import XGBRegressor
+from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge
 
-def get_validation_scores(models, X_train, y_train, folds, X_test = [], y_test = []):
+def get_validation_scores(models, X_train, y_train, folds):
     scores_val_mean = []
     scores_val_std = []
     scores_val = []
-    scores_test = []
     names = []
     for name, model in models:
         names.append(name)
@@ -26,14 +27,6 @@ def get_validation_scores(models, X_train, y_train, folds, X_test = [], y_test =
         scores_val.append(val_scores)
         scores_val_mean.append(val_scores.mean())
         scores_val_std.append(val_scores.std())
-        if len(X_test) != 0:
-            model.fit(X_train, y_train)
-            st = get_test_scores(model, X_test, y_test)
-            scores_test.append(st)
-    if len(X_test) != 0:
-        tab = pd.DataFrame({ "Model" : names, "Cross Validation (Mean)" : scores_val_mean, "Cross Validation (Std)" : scores_val_std, "Cross Validation (Scores)" : scores_val, "Test": scores_test })
-        tab.sort_values(by=['Test'], ascending = True, inplace = True)
-    else:
         tab = pd.DataFrame({ "Model" : names, "Cross Validation (Mean)" : scores_val_mean, "Cross Validation (Std)" : scores_val_std, "Cross Validation (Scores)" : scores_val })
         tab.sort_values(by=['Cross Validation (Mean)'], ascending = True, inplace = True)
     return(tab)
@@ -60,19 +53,21 @@ train.drop(['target'], axis=1, inplace = True)
 
 scaler = RobustScaler()
 
-threshold = .95 * (1 - .95)
+threshold = .98 * (1 - .98)
 variance = VarianceThreshold(threshold)
 
-anova_filter = SelectKBest(f_regression, k=500)
+anova_filter = SelectKBest(f_regression, k=4000)
 
-model_rforest = RandomForestRegressor(n_estimators = 200, 
-                                      max_features = 0.75,
-                                      random_state=0,
-                                      max_depth=15)
+model_rforest = RandomForestRegressor(n_estimators = 50, 
+                                      max_features = 0.7,
+                                      random_state=2018,
+                                      max_depth=20,
+                                      min_samples_leaf = 4,
+                                      min_samples_split = 10)
 
 model_lgb = lgb.LGBMRegressor(objective='regression',
                               metric="rmse",
-                              n_estimators = 150,
+                              n_estimators = 500,
                               num_leaves = 30,
                               learning_rate = 0.01,
                               bagging_fraction = 0.7,
@@ -81,7 +76,14 @@ model_lgb = lgb.LGBMRegressor(objective='regression',
                               bagging_seed = 2018,
                               verbosity = -1)
 
-percentile = SelectPercentile(mutual_info_regression, percentile=50)
+model_xgb = XGBRegressor(n_estimators = 100, 
+                         colsample_bytree = 0.7,
+                         colsample_bylevel = 0.7,
+                         learning_rate=0.1)
+
+model_byr = BayesianRidge()
+
+percentile = SelectPercentile(mutual_info_regression, percentile=85)
 
 from_model = SelectFromModel(ExtraTreesRegressor(n_estimators=200, max_depth=20, max_features=0.5, n_jobs=-1, random_state=0))
 
@@ -92,11 +94,11 @@ feature_selection_union = FeatureUnion(feature_selection)
 
 estimators = []
 estimators.append(('low_variance', variance))
-estimators.append(('scaler', scaler))
+#estimators.append(('scaler', scaler))
 #estimators.append(('anova', anova_filter))
 estimators.append(('log_transform', log_transformer))
-estimators.append(('percentile', percentile))
-estimators.append(('from_model', from_model))
+#estimators.append(('percentile', percentile))
+#estimators.append(('from_model', from_model))
 #estimators.append(('feature_selection', feature_selection_union))
 
 pipe = Pipeline(estimators)
@@ -114,9 +116,14 @@ model_lgb.fit(train_X_reduced, train_y)
 
 prediction = model_lgb.predict(test_X_reduced)
 
-score = pp.score_model(model_lgb, train_X_reduced, train_y)
-print(score.mean())
-print(score.std())
+tree_models = []
+tree_models.append(("lgb", model_lgb))
+#tree_models.append(("rf", model_rforest))
+#tree_models.append(("xgb", model_xgb))
+tree_models.append(("byr", model_byr))
+
+cross_val_table = get_validation_scores(tree_models, train_X_reduced, train_y, 5)
+print(cross_val_table)
 
 
 make_submission(model_lgb, train_X_reduced, train_y, test_X_reduced, ids, filename = 'submission.csv')
